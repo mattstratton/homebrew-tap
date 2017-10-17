93f0791d02f7a783894be641cdc76411c312f0a9
goreleaserbot <goreleaser@carlosbecker.com>
2017-10-16 21:30:48 -0500

    bowie version 0.1.5

--- bowie.rb
+++ bowie.rb
@@ -1,9 +1,9 @@
 class Bowie < Formula
   desc "A pretty changelong generator"
   homepage "https://github.com/mattstratton/bowie"
-  url "https://github.com/mattstratton/bowie/releases/download/0.1.4/bowie_0.1.4_darwin-amd64.tar.gz"
-  version "0.1.4"
-  sha256 "4feedaa1d122752f16a76a2417baf80dd30cea87fff0777327615e3dafe9dcd3"
+  url "https://github.com/mattstratton/bowie/releases/download/0.1.5/bowie_0.1.5_darwin-amd64.tar.gz"
+  version "0.1.5"
+  sha256 "4c5dd0be35489f20288870add8f7abb5715f210685794a09a04a38aba9d8008d"
 
   depends_on "git"
 
